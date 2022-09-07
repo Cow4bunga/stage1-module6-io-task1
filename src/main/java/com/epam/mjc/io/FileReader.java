a9f830422660fb2fc40b8ec9c9d6a22fedd7bf33
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-07 13:04:59 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,62 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        BufferedReader inputStream=null;
+        Profile profile=new Profile();
+
+        try {
+            inputStream=new BufferedReader(new java.io.FileReader(file));
+            String str;
+            Map<String,String> map=new HashMap<>();
+
+            while((str=inputStream.readLine())!=null){
+                String[] splitted=str.split(": ");
+                map.put(splitted[0],splitted[1]);
+            }
+
+            for (String s : map.keySet()) {
+                switch (s){
+                    case "Name":
+                        profile.setName(map.get(s));
+                        break;
+                    case "Email":
+                        profile.setEmail(map.get(s));
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(map.get(s)));
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(map.get(s)));
+                        break;
+                    default:
+                        System.out.println("Illegal input!");
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(inputStream!=null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return profile;
     }
+
+//    public static void main(String[] args) {
+//        FileReader fileReader=new FileReader();
+//        Profile profile=fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
+//        System.out.println(profile);
+//    }
 }

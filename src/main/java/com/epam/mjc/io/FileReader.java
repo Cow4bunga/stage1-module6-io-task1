c3735167d55527ed3f891c4dfb1c2150628f43d0
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-07 21:55:50 +0300

    done(cleaned up, try number 4)

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,26 +6,31 @@ public class FileReader {
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
         try (BufferedReader inputStream = new BufferedReader(new java.io.FileReader(file))) {
-            String str;
+            String line;
             String[] tokens;
-            while ((str = inputStream.readLine()) != null) {
-                tokens = str.split(": ");
+            while ((line = inputStream.readLine()) != null) {
+                tokens = line.split(": ");
                 switch (tokens[0]) {
-                    case "Name":
+                    case "Name": {
                         profile.setName(tokens[1]);
                         break;
-                    case "Email":
+                    }
+                    case "Email": {
                         profile.setEmail(tokens[1]);
                         break;
-                    case "Age":
+                    }
+                    case "Age": {
                         profile.setAge(Integer.parseInt(tokens[1]));
                         break;
-                    case "Phone":
+                    }
+                    case "Phone": {
                         profile.setPhone(Long.parseLong(tokens[1]));
                         break;
-                    default:
+                    }
+                    default: {
                         System.out.println("Illegal input!");
                         break;
+                    }
                 }
             }
         } catch (IOException e) {
